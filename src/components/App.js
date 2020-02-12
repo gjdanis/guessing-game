358757d99f93a6d167bfb00ad4e17d1002105cfe
Joe Danis <gdanis@wayfair.com>
2020-02-12 14:29:37 -0500

    Initial working version

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
 
-const MIN_NUMBER = 1
-const MAX_NUMBER = 1000
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 1000;
+const MAX_GUESSES = 11;
 
-class Guess extends Component {
-  render () {
+const average = (a, b) => Math.floor((a + b) / 2);
+
+class ComputerGuesser extends Component {
+  state = {
+    bestMin: MIN_NUMBER,
+    bestMax: MAX_NUMBER,
+    guesses: []
+  }
+
+  lastGuess = () => this.state.guesses[this.state.guesses.length - 1];
+  isGameStarted = () => this.state.guesses.length > 0;
+  isGameOver = () => this.state.guesses.length === MAX_GUESSES;
+
+  onRestartGame = () => {
+    const state = {
+      bestMin: MIN_NUMBER,
+      bestMax: MAX_NUMBER,
+      guesses: []
+    }
+
+    this.setState(state, this.guess);
+  }
+
+  guess() {
+    const bestGuess = average(this.state.bestMin, this.state.bestMax);
+    this.setState({ guesses: this.state.guesses.concat(bestGuess) });
+  }
+
+  onGuessHigher = () => this.setState({bestMin: this.lastGuess()}, this.guess);
+  onGuessLower = () => this.setState({bestMax: this.lastGuess()}, this.guess);
+
+  render() {
+    if (this.isGameOver()) {
+      return (
+        <button onClick={this.onRestartGame}>New Game</button>
+      );
+    }
+    
+    if (this.isGameStarted()) {
+      const lastGuess = this.state.guesses[this.state.guesses.length - 1];
+
+      return (
+        <div>
+          <h2>Is your number {lastGuess}?</h2>
+          <button onClick={this.onGuessHigher}>Higher</button>
+          <button onClick={this.onGuessLower}>Lower</button>
+          <button onClick={this.onRestartGame}>Yes that's it!</button>
+        </div>
+      );
+    }
+
     return (
-      <div>
-        Is your number {this.props.number}?
-      </div>
+      <button onClick={this.onRestartGame}>Start</button>
     );
   }
 }
@@ -16,7 +64,6 @@ class Guess extends Component {
 // need a guess button
 
 class App extends Component {
-  state = { guesses: [], bestMin: MIN_NUMBER, bestMax: MAX_NUMBER }
 
   clickGuess = () => {
     const { bestMin, bestMax } = this.state
@@ -28,8 +75,8 @@ class App extends Component {
     return (
       <div>
         <h2>Guessing Game</h2>
-        <Guess number={1} />
-        <Guess number={2} />
+        <p>Think of a number between 1 and 1000. I'll try and guess it!</p>
+        <ComputerGuesser />
       </div>
     );
   }
